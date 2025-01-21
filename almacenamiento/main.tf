9ab6885d2b53c65103aca81979badec36659ce18
aminespinoza10 <amin.espinoza@outlook.com>
2025-01-21 12:15:38 -0500

    Ejercicio de almacenamiento actualizado

---
+++ almacenamiento/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.16.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "30a83aff-7a8b-4ca3-aa48-ab93268b5a8b"
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = "brazilsouth"
+  name = "grupo_De_Almacenamiento"
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "amintestplatzi"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  public_network_access_enabled = false
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+output "cadena_conexion" {
+  value = azurerm_storage_account.storage_account.primary_connection_string
+  sensitive = true
+}
