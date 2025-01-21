a91096657065812f79ac416a19bc60401c55517b
aminespinoza10 <amin.espinoza@outlook.com>
2025-01-21 12:31:29 -0500

    Ejercicio de proveedores agregado

---
+++ proveedores/main.tf
@@ -0,0 +1,34 @@
+resource "azurecaf_name" "rg_name" {
+  name          = "amines"
+  resource_type = "azurerm_resource_group"
+  prefixes      = ["dev"]
+  suffixes      = ["y", "z"]
+  random_length = 3
+  clean_input   = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = "brazilsouth"
+  name = azurecaf_name.rg_name.result
+}
+
+resource "azurecaf_name" "storage_name" {
+  name          = "amines"
+  resource_type = "azurerm_storage_account"
+  prefixes      = ["dev"]
+  random_length = 3
+  clean_input   = true
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = azurecaf_name.storage_name.result
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  public_network_access_enabled = false
+
+  tags = {
+    environment = "staging"
+  }
+}
