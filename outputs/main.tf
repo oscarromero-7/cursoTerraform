2d9abbd5bef72a59b82e137a52ee97e1c387d6a1
aminespinoza10 <amin.espinoza@outlook.com>
2025-01-21 10:36:48 -0500

    Ejercicio de valores de salida agregado

---
+++ outputs/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.16.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "30a83aff-7a8b-4ca3-aa48-ab93268b5a8b"
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = "mexicocentral"
+  name = var.rg_nombre
+}
---
+++ outputs/outputs.tf
@@ -0,0 +1,3 @@
+output "nombre_rg_output" {
+  value = azurerm_resource_group.rg.id
+}
